3cfdf8ff4db6f22c38e859c5bf4b7e6b0ae6eae0
aidencachia <147393490+aidencachia@users.noreply.github.com>
2024-09-13 11:39:35 +0200

    Refactor the creation of the GameObjects to their corresponding method

--- App.cpp
+++ App.cpp
@@ -13,15 +13,6 @@ namespace appSpace{
     App::~App() {}
     
     void App::run() {
-        std::shared_ptr<graphics::Model> circleModel = graphics::Model::createCircleModel(device, 64);
-        std::vector<graphics::GameObject> physicsObjects{};
-        auto red = graphics::GameObject::createRigidbodyObject();
-        red.transform2d.scale = glm::vec2{.2};
-        red.transform2d.translation = {.0, -.5};
-        red.color = {1.f, 0.f, 0.f};
-        red.rigidBody2d.velocity = {.001f, .0f};
-        red.model = circleModel;
-        physicsObjects.push_back(std::move(red));
         
         graphics::FirstRenderSystem renderSystem{device, renderer.getSwapChainRenderPass()};
         
@@ -31,12 +22,12 @@ namespace appSpace{
             glfwPollEvents();
             if(auto commandBuffer = renderer.beginFrame()){
                 
-                for (graphics::GameObject& obj: physicsObjects) {
+                for (graphics::GameObject& obj: gameObjects) {
                     obj.update(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - prevTime));
                 }
                 
                 renderer.beginSwapChainRenderPass(commandBuffer);
-                renderSystem.renderGameObjects(commandBuffer, physicsObjects);
+                renderSystem.renderGameObjects(commandBuffer, gameObjects);
                 renderer.endSwapChainRenderPass(commandBuffer);
                 renderer.endFrame();
             }
@@ -46,21 +37,15 @@ namespace appSpace{
     }
     
     void App::loadGameObjects() {
-        std::vector<graphics::Model::Vertex> vertices{
-                {{.0,  -.5}, {1.0, .0,  .0}},
-                {{.5,  .5},  {.0,  1.0, .0}},
-                {{-.5, .5},  {.0,  .0,  1.0}}};
-
-        auto model = std::make_shared<graphics::Model>(device, vertices);
+        std::shared_ptr<graphics::Model> circleModel = graphics::Model::createCircleModel(device, 64);
         
-        auto triangle = graphics::GameObject::createGameObject();
-        triangle.model = model;
-        triangle.color = {.1, .8, .1};
-        triangle.transform2d.translation.x = .2f;
-        triangle.transform2d.scale = {2.f, .5f};
-        triangle.transform2d.rotation = .25f * glm::two_pi<float>();
-        
-        gameObjects.push_back(std::move(triangle));
+        auto red = graphics::GameObject::createRigidbodyObject();
+        red.transform2d.scale = glm::vec2{.2};
+        red.transform2d.translation = {.0, -.5};
+        red.color = {1.f, 0.f, 0.f};
+        red.rigidBody2d.velocity = {.001f, .0f};
+        red.model = circleModel;
+        gameObjects.push_back(std::move(red));
     }
     
 }
